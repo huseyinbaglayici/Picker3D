43169ff5ea2f9b9f2771465ff3eb6c7f5abe77a9
huseyinbaglayici <huseyinow35@hotmail.com>
2025-04-23 02:39:16 +0300

    Input System Initialized

---
+++ Assets/Scipts/Data/UnityObjects/CD_Input.cs
@@ -0,0 +1,11 @@
+﻿using Scipts.Data.ValueObjects;
+using UnityEngine;
+
+namespace Scipts.Data.UnityObjects
+{
+    [CreateAssetMenu(fileName = "CD_Input", menuName = "Picker3D/CD_Input", order = 0)]
+    public class CD_Input : ScriptableObject
+    {
+        public InputData Data;
+    }
+}
---
+++ Assets/Scipts/Data/ValueObjects/InputData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace Scipts.Data.ValueObjects
+{
+    [Serializable]
+    public struct InputData
+    {
+        public float HorizontalInputSpeed;
+        public Vector2 ClampValues;
+        public float ClampSpeed;
+    }
+}
