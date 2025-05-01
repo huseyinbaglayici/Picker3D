1c2ee191dd6cccf4e5859ee24693fe7499038b4f
huseyinbaglayici <huseyinow35@hotmail.com>
2025-05-01 12:43:10 +0300

    Camera System Finalized

---
+++ Assets/Scripts/Runtime/Managers/CameraManager.cs
@@ -0,0 +1,73 @@
+﻿using Cinemachine;
+using Runtime.Signals;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Runtime.Managers
+{
+    public class CameraManager : MonoBehaviour
+    {
+        #region Self Variables
+
+        #region Serialized Variables
+
+        [SerializeField] private CinemachineVirtualCamera _virtualCamera;
+
+        #endregion
+
+        #region Private Variables
+
+        private float3 _firstPosition;
+
+        #endregion
+
+        #endregion
+
+        private void Start()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            _firstPosition = transform.position;
+        }
+
+        private void OnEnable()
+        {
+            SubscribeEvents();
+        }
+
+        private void SubscribeEvents()
+        {
+            CameraSignals.Instance.onSetCameraTarget += OnSetCameraTarget;
+            CoreGameSignals.Instance.onReset += OnReset;
+        }
+
+
+        private void OnSetCameraTarget()
+        {
+            //var player = FindObjectOfType<PlayerManager>().transform;
+            //_virtualCamera.Follow = player;
+            // _virtualCamera.LookAt = player;
+        }
+
+
+        private void OnReset()
+        {
+            transform.position = _firstPosition;
+        }
+
+
+        private void UnSubscribeEvents()
+        {
+            CameraSignals.Instance.onSetCameraTarget -= OnSetCameraTarget;
+            CoreGameSignals.Instance.onReset -= OnReset;
+        }
+
+        private void OnDisable()
+        {
+            UnSubscribeEvents();
+        }
+    }
+}
